29d7ea41c37d78f14b337d438f183af390d82117
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 13:02:56 -0600

    Post and render item on submit

--- public/app.js
+++ public/app.js
@@ -2,6 +2,32 @@ $('.toggle-garage').on('click', () => {
   $('.garage').show()
 })
 
+$('.submit-item-button').on('click', (e) => {
+  e.preventDefault()
+  fetch(`http://localhost:3000/items`,
+      {
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: $('.name-input').val(),
+          reason: $('.reason-input').val(),
+          cleanliness: $('.cleanliness-input').val()
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        $('.show-items').append(`
+          <article id="${response.id}"
+             class="response-article">
+             <h2>${response.name}</h2>
+             <p>${response.reason}</p>
+             <p>${response.cleanliness}</p>
+          </article>`)
+      })
+})
+
 $(() => {
   $('.garage').hide()
   fetch(`http://localhost:3000/items`)
@@ -11,7 +37,7 @@ $(() => {
         $('.show-items').append(`
           <article id="${item.id}"
              class="item-article">
-             <p>${item.name}</p>
+             <h2>${item.name}</h2>
              <p>${item.reason}</p>
              <p>${item.cleanliness}</p>
           </article>`)
