07324ae71ebf00d1adecf292067a50bf9d777edf
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 12:35:31 -0600

    Fetch items on load

--- public/app.js
+++ public/app.js
@@ -1,7 +1,21 @@
-$('.show-items').on('click', () => {
+$('.toggle-garage').on('click', () => {
   $('.garage').show()
 })
 
 $(() => {
   $('.garage').hide()
+  fetch(`http://localhost:3000/items`)
+    .then(response => response.json())
+    .then(response => {
+      response.forEach(item => {
+        $('.show-items').append(`
+          <article id="${item.id}"
+             class="item-article">
+             <p>${item.name}</p>
+             <p>${item.reason}</p>
+             <p>${item.cleanliness}</p>
+          </article>`)
+      })
+    })
+
 })
