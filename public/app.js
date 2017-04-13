a4b4d58f4874a9d496e45795aadcf8c123f361fa
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 13:32:57 -0600

    Refactor storage to  be constructor for clarity and testing

--- public/app.js
+++ public/app.js
@@ -1,47 +1,67 @@
-$('.toggle-garage').on('click', () => {
-  $('.garage').show()
-})
+const Storage = function () {
+  this.garageDoor = $('.toggle-garage')
+  this.garage = $('.garage')
+  this.nameInput = $('.name-input')
+  this.reasonInput = $('.reason-input')
+  this.cleanlinessInput = $('.cleanliness-input')
+  this.submitItemButton = $('.submit-item-button')
+  this.showItems = $('.show-items')
+  return this
+}
 
-$('.submit-item-button').on('click', (e) => {
-  e.preventDefault()
-  fetch(`http://localhost:3000/items`,
-      {
-        method:'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: $('.name-input').val(),
-          reason: $('.reason-input').val(),
-          cleanliness: $('.cleanliness-input').val()
-        })
-      })
-      .then(response => response.json())
-      .then(response => {
-        $('.show-items').append(`
-          <article id="${response.id}"
-             class="response-article">
-             <h2>${response.name}</h2>
-             <p>${response.reason}</p>
-             <p>${response.cleanliness}</p>
-          </article>`)
-      })
-})
-
-$(() => {
-  $('.garage').hide()
+Storage.prototype.loadItems = () => {
   fetch(`http://localhost:3000/items`)
     .then(response => response.json())
     .then(response => {
       response.forEach(item => {
-        $('.show-items').append(`
-          <article id="${item.id}"
-             class="item-article">
-             <h2>${item.name}</h2>
-             <p>${item.reason}</p>
-             <p>${item.cleanliness}</p>
-          </article>`)
+        storage.renderItem(item)
       })
     })
+}
 
+Storage.prototype.addItem = (name, reason, cleanliness) => {
+  fetch(`http://localhost:3000/items`,
+    {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        reason: reason,
+        cleanliness: cleanliness
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      storage.renderItem(response)
+      })
+}
+
+Storage.prototype.renderItem = (response) => {
+  storage.showItems.append(`
+      <article id="${response.id}"
+        class="response-article">
+        <h2>${response.name}</h2>
+        <p>${response.reason}</p>
+        <p>${response.cleanliness}</p>
+      </article>
+      `)
+}
+
+const storage = new Storage
+
+storage.submitItemButton.on('click', (e) => {
+  e.preventDefault()
+  storage.addItem(storage.nameInput.val(), storage.reasonInput.val(), storage.cleanlinessInput.val())
+})
+
+storage.garageDoor.on('click', () => {
+  storage.garage.show()
+})
+
+
+$(() => {
+  storage.garage.hide()
+  storage.loadItems()
 })
