4194720ec137265caedd75d014ea855bb5acbd3b
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 10:33:34 -0600

    Add get endpoint for specific items

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ app.set('port', process.env.PORT || 3000)
 
 app.locals.title = 'Mysterious garage'
 app.locals.items = [
-  { id: 1,
+  { id: '1',
   name: 'Hubcap',
   reason: 'Future mixed media project',
   cleanliness: 'Rancid'
   },
-  { id: 2,
+  { id: '2',
    name: 'Curtains',
    reason: 'Cool pattern',
    cleanliness: 'Dusty'
@@ -19,6 +19,14 @@ app.locals.items = [
 
 app.get('/', (request, response) => {
   response.send(app.locals.items)
+})
+
+app.get('/items/:id', (request, response) => {
+  const { id } = request.params
+  const item = app.locals.items.find(item => item.id === id)
+
+  if (!item) return response.sendStatus(404)
+  response.send(item)
 })
 
 app.listen(app.get('port'), () => {
