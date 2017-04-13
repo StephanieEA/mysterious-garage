c12cba9158273cb507d4e3fa902d75013cc338f3
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 14:37:24 -0600

    update item with put from ui

--- public/app.js
+++ public/app.js
@@ -6,6 +6,7 @@ const Storage = function () {
   this.cleanlinessInput = $('.cleanliness-input')
   this.submitItemButton = $('.submit-item-button')
   this.showItems = $('.show-items')
+  this.updateCleanliness = $('.update-cleanliness')
   return this
 }
 
@@ -13,6 +14,7 @@ Storage.prototype.loadItems = () => {
   fetch(`http://localhost:3000/items`)
     .then(response => response.json())
     .then(response => {
+      console.log(response)
       response.forEach(item => {
         storage.renderItem(item)
       })
@@ -38,13 +40,29 @@ Storage.prototype.addItem = (name, reason, cleanliness) => {
       })
 }
 
+Storage.prototype.updateItem = (id, cleanliness) => {
+  fetch(`http://localhost:3000/items/${id}`,
+    {
+      method:'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cleanliness: cleanliness
+      })
+    })
+  .then(response => console.log(response))
+}
+
 Storage.prototype.renderItem = (response) => {
   storage.showItems.append(`
       <article id="${response.id}"
         class="response-article">
         <h2>${response.name}</h2>
         <p>${response.reason}</p>
-        <p>${response.cleanliness}</p>
+        <p contenteditable>${response.cleanliness}
+          <button class='update-cleanliness'>update</button>
+        </p>
       </article>
       `)
 }
@@ -54,6 +72,13 @@ const storage = new Storage
 storage.submitItemButton.on('click', (e) => {
   e.preventDefault()
   storage.addItem(storage.nameInput.val(), storage.reasonInput.val(), storage.cleanlinessInput.val())
+})
+
+storage.garage.on('click', '.update-cleanliness', (e) => {
+  const id = e.target.closest('article').id
+  const cleanliness = e.target.closest('p').innerText
+
+  storage.updateItem(id, cleanliness)
 })
 
 storage.garageDoor.on('click', () => {
