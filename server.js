d2ef1206cd2e87600a5fb3c376ec0f6bb993d3a6
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 11:00:40 -0600

    Add endpoint to post item

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 3000)
 
@@ -17,8 +21,8 @@ app.locals.items = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send(app.locals.items)
+app.get('/items', (request, response) => {
+  response.json(app.locals.items)
 })
 
 app.get('/items/:id', (request, response) => {
@@ -26,7 +30,21 @@ app.get('/items/:id', (request, response) => {
   const item = app.locals.items.find(item => item.id === id)
 
   if (!item) return response.sendStatus(404)
-  response.send(item)
+  response.json(item)
+})
+
+app.post('/items', (request, response) => {
+  const { name, reason, cleanliness } = request.body
+  const id = Date.now()
+
+  if (!name || !reason || !cleanliness) {
+    return response.status(422).send({
+      error: 'All item properties are not provided'
+    })
+  }
+
+  app.locals.items.push({ id, name, reason, cleanliness })
+  response.status(201).json({ id, name, reason, cleanliness })
 })
 
 app.listen(app.get('port'), () => {
