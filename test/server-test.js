435a0d066ff71db292159c622637975ea4273204
Stephanie Andrews <seandrews11@gmail.com>
2017-04-13 23:16:05 -0600

    add skeleton for tests

---
+++ test/server-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = chai.expect
+const assert = chai.assert
+const should = chai.should()
+const app = require('../server.js')
+
+chai.use(chaiHttp)
+
+describe('Server', function() {
+  it('should exist', function() {
+    expect(app).to.exist;
+  });
+});
